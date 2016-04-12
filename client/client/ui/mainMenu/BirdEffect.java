44b66c73cc35868253ab0384cf5e552c02fdc33b
itggot-jakob-frykmer <itggot-jakob-frykmer@users.noreply.github.com>
2016-04-12 23:55:18 +0200

    Added files via upload

---
+++ client/client/ui/mainMenu/BirdEffect.java
@@ -0,0 +1,20 @@
+package client.ui.mainMenu;
+
+public class BirdEffect {
+
+	private int x;
+	private int y;
+
+	private double scale = 1;
+	private int width = (int) (269 * scale);
+	private int height = (int) (501 * scale);
+
+	public BirdEffect(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void update() {
+		
+	}
+}
